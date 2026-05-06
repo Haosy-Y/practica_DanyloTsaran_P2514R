befc817566b16244216510ae7744218a55e93d25
hao <haosybtw@gmail.com>
2026-05-06 09:46:13 +0300

    06.05.2026 -> Создана база для структур, запись информации в структуру(вектор)

--- variant33/C++/main.cpp
+++ variant33/C++/main.cpp
@@ -3,20 +3,40 @@ using namespace std;
 struct Films {
     int id;
     string name, genre;
-    time_t duration;
+    string duration;
     string agelimit;
 };
+struct Sessions {
+    int sid;
+    int filmId;
+    string date;
+    string time;
+    int hallId;
+    int price;
+};
+
 
 int main() {
-    ifstream file("movie.txt");
-    Films s[100];
-    int n = 0;
-
-    while(file >> s[n].id >> s[n].name >> s[n].genre >> s[n].duration >> s[n].agelimit) {
-        
-        n++;
+    ifstream file1("movie.txt");
+    ifstream file2("session.txt");
+    if (!file1.is_open() || !file2.is_open()) {
+        cout << "Ошибка: не удалось открыть movie.txt или session.txt\n";
+        return 1;
     }
+
+    vector<Films> films;
+    vector<Sessions> sessions;
+
+    Films film;
+    while (file1 >> film.id >> film.name >> film.genre >> film.duration >> film.agelimit) {
+        films.push_back(film);
+    }
+
+    Sessions session;
+    while (file2 >> session.sid >> session.filmId >> session.date >> session.time >> session.hallId >> session.price) {
+        sessions.push_back(session);
+    }
+
+
     return 0;
 } 
-
-
