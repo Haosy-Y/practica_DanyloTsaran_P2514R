2808bad64749d169e543744b43cf9f85b6ab9113
hao <haosybtw@gmail.com>
2026-05-06 17:43:50 +0300

    06.05.2026 -> созданы функции 1-4 и перенесен вывод интерфейса в отдельную функцию

--- variant33/C++/main.cpp
+++ variant33/C++/main.cpp
@@ -1,42 +1,164 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 struct Films {
-    int id;
-    string name, genre;
-    string duration;
-    string agelimit;
+  int id;
+  string name, genre;
+  string duration;
+  string agelimit;
 };
 struct Sessions {
-    int sid;
-    int filmId;
-    string date;
-    string time;
-    int hallId;
-    int price;
+  int sid;
+  int filmId;
+  string date;
+  string time;
+  int hallId;
+  int price;
 };
 
+void printFilms(vector<Films> films) {
+  for (int i = 0; i < films.size(); i++) {
+    cout << films[i].id << " " << films[i].name << " " << films[i].genre << " "
+         << films[i].duration << " " << films[i].agelimit << endl;
+  }
+}
+
+void printSessions(vector<Sessions> sessions) {
+  for (int i = 0; i < sessions.size(); i++) {
+    cout << sessions[i].sid << " " << sessions[i].filmId << " "
+         << sessions[i].date << " " << sessions[i].time << " "
+         << sessions[i].hallId << " " << sessions[i].price << endl;
+  }
+}
+
+void addFilm(vector<Films> &films, Films film) {
+  cout << "Введите фильм в формате: id name genre h:mm:ss ageLimit\n";
+  cout << "Пример: 1 The_Shawshank_Redemption Prison_Drama 2:22:25 R\n";
+
+  string line;
+  getline(cin >> ws, line);
+
+  regex fullPattern(
+      R"(^([0-9]+)\s+([A-Za-z_]+)\s+([A-Za-z_]+)\s+([0-9]{1,2}:[0-9]{2}:[0-9]{2})\s+(G|PG|PG-13|R|NC-17)$)");
+  smatch match;
+  if (!regex_match(line, match, fullPattern)) {
+    cout << "Фильм не добавлен - неверный формат" << endl;
+    return;
+  }
+
+  film.id = stoi(match[1].str());
+  film.name = match[2].str();
+  film.genre = match[3].str();
+  film.duration = match[4].str();
+  film.agelimit = match[5].str();
+
+  int h, m, s;
+  char c1, c2;
+  stringstream ss(film.duration);
+  ss >> h >> c1 >> m >> c2 >> s;
+  if (ss.fail() || c1 != ':' || c2 != ':' || m > 59 || s > 59) {
+    cout << "Фильм не добавлен - неверный формат" << endl;
+    return;
+  }
+  cout << "Фильм добавлен" << endl;
+  films.push_back(film);
+}
+
+void addSession(vector<Sessions> &sessions, Sessions session) {
+  cout << "Введите сеанс в формате: sid filmId date time hallId price\n";
+  cout << "Пример: 1 1 2024-01-01 12:00 1 100\n";
+
+  string line;
+  getline(cin >> ws, line);
+
+  regex fullPattern(
+      R"(^([0-9]+)\s+([0-9]+)\s+([0-9]{4}-[0-9]{2}-[0-9]{2})\s+([0-9]{2}:[0-9]{2})\s+([0-9]+)\s+([0-9]+)$)");
+  smatch match;
+  if (!regex_match(line, match, fullPattern)) {
+    cout << "Сеанс не добавлен - неверный формат" << endl;
+    return;
+  }
+
+  session.sid = stoi(match[1].str());
+  session.filmId = stoi(match[2].str());
+  session.date = match[3].str();
+  session.time = match[4].str();
+  session.hallId = stoi(match[5].str());
+  session.price = stoi(match[6].str());
+
+  cout << "Сеанс добавлен" << endl;
+  sessions.push_back(session);
+}
+
+void runMenu(vector<Films> &films, vector<Sessions> &sessions) {
+  int choice;
+  Films film;
+  Sessions session;
+
+  while (true) {
+    cout << "\n========================================\n";
+    cout << "            МЕНЮ КИНОТЕАТРА\n";
+    cout << "========================================\n";
+    cout << "1. Показать все фильмы\n";
+    cout << "2. Показать все сеансы\n";
+    cout << "3. Добавить новый фильм\n";
+    cout << "4. Добавить новый сеанс\n";
+    cout << "0. Выход\n";
+    cout << "----------------------------------------\n";
+    cout << "Ваш выбор: ";
+
+    if (!(cin >> choice)) {
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << "Неверный выбор. Введите число от 0 до 4.\n";
+      continue;
+    }
+
+    switch (choice) {
+    case 1:
+      printFilms(films);
+      break;
+    case 2:
+      printSessions(sessions);
+      break;
+    case 3:
+      addFilm(films, film);
+      break;
+    case 4:
+      addSession(sessions, session);
+      break;
+    case 0:
+      cout << "Выход из программы.\n";
+      return;
+    default:
+      cout << "Неверный выбор. Введите число от 0 до 4.\n";
+      break;
+    }
+  }
+}
 
 int main() {
-    ifstream file1("movie.txt");
-    ifstream file2("session.txt");
-    if (!file1.is_open() || !file2.is_open()) {
-        cout << "Ошибка: не удалось открыть movie.txt или session.txt\n";
-        return 1;
-    }
+  ifstream file1("movie.txt");
+  ifstream file2("session.txt");
+  if (!file1.is_open() || !file2.is_open()) {
+    cout << "Ошибка: не удалось открыть movie.txt или session.txt\n";
+    return 1;
+  }
 
-    vector<Films> films;
-    vector<Sessions> sessions;
+  vector<Films> films;
+  vector<Sessions> sessions;
 
-    Films film;
-    while (file1 >> film.id >> film.name >> film.genre >> film.duration >> film.agelimit) {
-        films.push_back(film);
-    }
+  Films film;
+  while (file1 >> film.id >> film.name >> film.genre >> film.duration >> film.agelimit) {
+    films.push_back(film);
+  }
 
-    Sessions session;
-    while (file2 >> session.sid >> session.filmId >> session.date >> session.time >> session.hallId >> session.price) {
-        sessions.push_back(session);
-    }
+  Sessions session;
+  while (file2 >> session.sid >> session.filmId >> session.date >>
+         session.time >> session.hallId >> session.price) {
+    sessions.push_back(session);
+  }
+  runMenu(films, sessions);
 
-
-    return 0;
-} 
+  return 0;
+}
